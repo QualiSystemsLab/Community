9e15e4bd9fd9a0a978b61fcb5d6af3d1fd65fa91
Cody <cojacfar@gmail.com>
2021-11-28 02:35:33 -0800

    Remove sudo command

--- terraform/aws-document-db/insert_data.sh
+++ terraform/aws-document-db/insert_data.sh
@@ -10,7 +10,7 @@ DATA="$6"
 # install mongoimport cli tool
 echo 'Install MongoDB'
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc |  apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" |  tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 apt-get update -y
 apt-get install -y mongodb-org
