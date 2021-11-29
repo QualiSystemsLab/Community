3cc6e94dc6f2956411d7bc17305b980f4780b220
Cody <cojacfar@gmail.com>
2021-11-29 10:14:36 -0800

    Removing sudo command

--- terraform/aws-document-db/insert_data.sh
+++ terraform/aws-document-db/insert_data.sh
@@ -12,7 +12,7 @@ OS=`cat /etc/os-release`
 echo 'Install MongoDB'
 apt-get update -y
 apt-get install gnupg
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
 echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 apt-get update
 apt-get install -y mongodb-org
