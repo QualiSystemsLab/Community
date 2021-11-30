3dd1b42624d3776bc7299efc76bfd73f2b5ea4c9
Cody <cojacfar@gmail.com>
2021-11-30 13:29:41 -0800

    Changing allowed CIDR

--- terraform/aws-document-db/main.tf
+++ terraform/aws-document-db/main.tf
@@ -38,7 +38,8 @@ resource "aws_security_group" "docdb_sg" {
     from_port   = 27017
     to_port     = 27017
     protocol    = "tcp"
-    cidr_blocks = ["${data.aws_vpc.sandbox_vpc.cidr_block}"]
+    #cidr_blocks = ["${data.aws_vpc.sandbox_vpc.cidr_block}"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
